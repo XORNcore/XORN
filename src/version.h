73368a433e1b09af849d02c991768454d63ff135
XORNcore <41155603+XORNcore@users.noreply.github.com>
2019-02-05 23:03:45 -0500

    Fake PoS fix v1.1.1

--- src/version.h
+++ src/version.h
@@ -12,7 +12,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70901;
+static const int PROTOCOL_VERSION = 70902;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -23,6 +23,7 @@ static const int GETHEADERS_VERSION = 70077;
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70900;
 static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70901;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT_POSFIX = 70902;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
